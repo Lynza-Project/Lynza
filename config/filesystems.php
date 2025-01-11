123db39bb8042fef77f4f74e7fc21db3bc762e66
Ayatooo <l.reynard@isi-dsi.fr>
2025-01-11 13:35:37 +0100

    :rocket: main - packages imported

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,6 +57,16 @@ return [
             'throw' => false,
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('MINIO_KEY'),
+            'secret' => env('MINIO_SECRET'),
+            'region' => env('MINIO_REGION'),
+            'bucket' => env('MINIO_BUCKET'),
+            'url' => env('MINIO_ENDPOINT'),
+            'endpoint' => env('MINIO_ENDPOINT'),
+            'use_path_style_endpoint' => env('MINIO_USE_PATH_STYLE_ENDPOINT', false),
+        ],
     ],
 
     /*
