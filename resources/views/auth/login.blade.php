e8cdb5709c4b23308741729ea41e68405bebb43d
Ayatooo <l.reynard@isi-dsi.fr>
2025-01-20 12:19:57 +0100

    :rocket: feature/auth - login implemented

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,66 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="h-screen flex flex-col lg:flex-row bg-gray-50">
+        <!-- Section image -->
+        <div
+            class="lg:w-1/2 lg:flex hidden justify-center items-center bg-cover bg-center"
+            style="background-image: url('{{ asset('img/university.jpg') }}');"
+        >
+        </div>
 
-    <!-- Register -->
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Vous n\'avez pas de compte?') }}
-        <a href="{{ route('register') }}" class="text-blue-600 hover:text-blue-500">
-            {{ __('Inscription') }}
-        </a>
+        <!-- Section formulaire -->
+        <div class="lg:w-1/2 flex justify-center items-center p-6">
+            <form method="POST" action="{{ route('login') }}"
+                  class="bg-white w-full md:w-3/4 lg:w-2/3 xl:w-1/2 p-8 shadow-lg rounded-md border">
+                @csrf
+                <h1 class="text-gray-800 font-bold text-2xl mb-1">Connexion</h1>
+                <p class="text-sm font-normal text-gray-600 mb-6">Veuillez vous connecter pour continuer</p>
+
+                <!-- Session Status -->
+                @if(session('status'))
+                    <div class="mb-4 text-green-600 font-semibold">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <!-- Email -->
+                <div class="mb-4">
+                    <x-input-label for="email" :value="__('Email')"/>
+                    <x-text-input id="email" class="block mt-1 w-full border-gray-300 rounded-md" type="email"
+                                  name="email" :value="old('email')" required autofocus autocomplete="username"/>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2"/>
+                </div>
+
+                <!-- Password -->
+                <div class="mb-4">
+                    <x-input-label for="password" :value="__('Mot de passe')"/>
+                    <x-text-input id="password" class="block mt-1 w-full border-gray-300 rounded-md" type="password"
+                                  name="password" required autocomplete="current-password"/>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2"/>
+                </div>
+
+                <!-- Remember Me -->
+                <div class="flex items-center mb-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox"
+                               class="rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500"
+                               name="remember">
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Se souvenir de moi') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex flex-col items-center">
+                    <!-- Button -->
+                    <button type="submit"
+                            class="bg-blue-500 w-full py-2 text-white rounded-md font-semibold hover:bg-blue-600 transition">
+                        {{ __('Connexion') }}
+                    </button>
+
+                    <!-- Link to register -->
+                    <p class="text-gray-600 text-sm mt-4 hover:underline">
+                        Pas encore de compte ? Contactez nous !
+                    </p>
+                </div>
+            </form>
+        </div>
     </div>
-
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Mot de passe')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Se souvenir de moi') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" href="{{ route('password.request') }}">
-                    {{ __('Mot de passe oublié ?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Connexion') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -58,7 +58,7 @@
     <div class="relative bg-blue-50">
         <!-- Image de fond -->
         <img
-            src="https://images.unsplash.com/20/cambridge.JPG?q=80&w=2047&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="{{ asset('img/university.jpg') }}"
             alt="Image de fond pour Lynza"
             class="absolute inset-0 w-full h-full object-cover"
         >
